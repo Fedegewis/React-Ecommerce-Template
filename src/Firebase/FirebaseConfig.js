eeb745246b3fcf39ca9d17f0c2210732a7e977e5
Fedegewis <Fedegewis@gmail.com>
2024-03-26 16:47:20 -0300

    Realizando inicializacion

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,14 +1,16 @@
 import firebase from "firebase";
 
+
+
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDRfeBFj2XHR7IWMAkPxLvU_LqN-CLMpWw",
+  authDomain: "brazeecommerce.firebaseapp.com",
+  databaseURL: "https://brazeecommerce-default-rtdb.firebaseio.com",
+  projectId: "brazeecommerce",
+  storageBucket: "brazeecommerce.appspot.com",
+  messagingSenderId: "1079638894169",
+  appId: "1:1079638894169:web:ae9506ba6c700679f17348"
+  //,measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
 const db = firebaseApp.firestore();
