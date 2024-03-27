8e0870c2e5fea9257bf38b9157672616e4aaee48
Fedegewis <Fedegewis@gmail.com>
2024-03-27 16:57:59 -0300

    Register

--- src/container/Login/Login.js
+++ src/container/Login/Login.js
@@ -9,6 +9,7 @@ function Login() {
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const loginUser = (event) => {
     event.preventDefault();
@@ -20,28 +21,46 @@ function Login() {
         })
         .catch((error) => {
           alert(
-            "Opps! something went wrong please check your console for more info"
+            "Oops! something went wrong. Please check your console for more info"
           );
           console.error(error.message);
         });
     } else {
-      alert("Please Enter all the fields");
+      alert("Please enter all the fields");
+    }
+  };
+
+  const registerUser = (event) => {
+    event.preventDefault();
+    if (email && password && confirmPassword && password === confirmPassword) {
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((authUser) => {
+          history.push("/");
+        })
+        .catch((error) => {
+          alert(
+            "Oops! something went wrong. Please check your console for more info"
+          );
+          console.error(error.message);
+        });
+    } else {
+      alert("Please enter all the fields correctly");
     }
   };
 
   return (
     <div className="login">
       <Container>
-        <Grid centered columns={3} doubling stackable>
+        <Grid centered columns={2} doubling stackable>
           <Grid.Column>
             <h2>Sign In</h2>
-
-            <Card>
+            <Card fluid>
               <Form className="login__form">
                 <Form.Field required>
                   <label>E-mail</label>
                   <input
-                    placeholder="First Name"
+                    placeholder="Email"
                     type="email"
                     onChange={(event) => setEmail(event.target.value)}
                   />
@@ -49,13 +68,48 @@ function Login() {
                 <Form.Field required>
                   <label>Password</label>
                   <input
-                    placeholder="password"
+                    placeholder="Password"
                     type="password"
                     onChange={(event) => setPassword(event.target.value)}
                   />
                 </Form.Field>
                 <Button color="green" type="submit" onClick={loginUser}>
                   Login
+                </Button>
+              </Form>
+            </Card>
+          </Grid.Column>
+
+          <Grid.Column style={{ marginBottom: "20px" }}>
+            <h2>Register</h2>
+            <Card fluid>
+              <Form className="register__form">
+                <Form.Field required>
+                  <label>E-mail</label>
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                </Form.Field>
+                <Form.Field required>
+                  <label>Password</label>
+                  <input
+                    placeholder="Password"
+                    type="password"
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </Form.Field>
+                <Form.Field required>
+                  <label>Confirm Password</label>
+                  <input
+                    placeholder="Confirm Password"
+                    type="password"
+                    onChange={(event) => setConfirmPassword(event.target.value)}
+                  />
+                </Form.Field>
+                <Button color="blue" type="submit" onClick={registerUser}>
+                  Register
                 </Button>
               </Form>
             </Card>
@@ -67,3 +121,4 @@ function Login() {
 }
 
 export default Login;
+
